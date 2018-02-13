7b79349ef1f4d2d3ff2ce495150b6aa79d7b22c9
Luxe <ogzg@protonmail.com>
2018-02-13 10:07:56 +0000

    Add files via upload

---
+++ pWAF.php
@@ -0,0 +1,262 @@
+<?php
+/**
+ *  pWAF 2.1 - Free Web Application Firewall, Open-Source.
+ *
+ *  @author Alemalakra
+ *  @modied Wrath
+ *  @version 2.1
+ */
+ 
+class pWAF {
+	function __construct() {
+		$this->IPHeader = "REMOTE_ADDR";
+		return true;
+	}
+	function vulnDetectedHTML($Method, $BadWord, $DisplayName, $TypeVuln) {
+
+	$line = $_COOKIE['username'] . " - $_SERVER[REMOTE_ADDR] - " . date('Y-m-d H:i:s') . " - " . $TypeVuln;
+	file_put_contents('spookies.log', $line . PHP_EOL, FILE_APPEND);?>
+	<!DOCTYPE html>
+<html lang="en" >
+
+<body>
+
+You was detected using <?echo $TypeVuln;?> by using <?echo $BadWord;?>
+
+</body>
+
+</html>
+
+		<?php die(); // Important... Never remove this, block request.
+	}
+	function getArray($Type) {
+		switch ($Type) {
+			case 'SQL':
+				return array(
+							"'",
+							'SELECT FROM',
+							'SELECT * FROM',
+							'ONION',
+							'union',
+							'UNION',
+							'UDPATE users SET',
+							'WHERE username',
+							'DROP TABLE',
+							'table',
+							'0x50',
+							'mid((select',
+							'union(((((((',
+							'concat(0x',
+							'0x3c62723e3c62723e3c62723e',
+							'0x3c696d67207372633d22',
+							'+#1q%0AuNiOn all#qa%0A#%0AsEleCt',
+							'unhex(hex(Concat(',
+							'Table_schema,0x3e,'
+							);
+				break;
+			case 'XSS':
+				return array('<img',
+						'img>',
+						'<image',
+						'document.cookie',
+						'onerror()',
+						'script>',
+						'<script',
+						'alert(',
+						'String.fromCharCode(',
+						'javascript:',
+						'onmouseover="',
+						'<BODY onload',
+						'svg onload');
+				break;
+			
+			default:
+				return false;
+				break;
+		}
+	}
+	function arrayFlatten(array $array) {
+	    $flatten = array();
+	    array_walk_recursive($array, function($value) use(&$flatten) {
+	        $flatten[] = $value;
+	    });
+
+	    return $flatten;
+	}
+	function sqlCheck($Value, $Method, $DisplayName) {
+		// For false alerts.
+		$Replace = array("can't" => "cant",
+						"don't" => "dont");
+		foreach ($Replace as $key => $value_rep) {
+			$Value = str_replace($key, $value_rep, $Value);
+		}
+		$BadWords = $this->getArray('SQL');
+		foreach ($BadWords as $BadWord) {
+			if (strpos(strtolower($Value), strtolower($BadWord)) !== false) {
+				// String contains some Vuln.
+				$this->vulnDetectedHTML($Method, $BadWord, $Value, 'SQL INJECTION');
+			}
+		}
+	}
+	function xssCheck($Value, $Method, $DisplayName) {
+		// For false alerts.
+		$Replace = array("<3" => ":heart:");
+		foreach ($Replace as $key => $value_rep) {
+			$Value = str_replace($key, $value_rep, $Value);
+		}
+		$BadWords = $this->getArray('XSS');
+
+		foreach ($BadWords as $BadWord) {
+			if (strpos(strtolower($Value), strtolower($BadWord)) !== false) {
+			    // String contains some Vuln.
+
+				$this->vulnDetectedHTML($Method, $BadWord, $DisplayName, 'XSS');
+
+			}
+		}
+	}
+	function is_html($string) {
+		return $string != strip_tags($string) ? true:false;
+
+	}
+	function santizeString($String) {
+		$String = escapeshellarg($String);
+		$String = htmlentities($String);
+		$XSS = $this->getArray('XSS');
+		foreach ($XSS as $replace) {
+			$String = str_replace($replace, '', $String);
+		}
+		$SQL = $this->getArray('SQL');
+		foreach ($SQL as $replace) {
+			$String = str_replace($replace, '', $String);
+		}
+		return $String;
+	}
+	function htmlCheck($value, $Method, $DisplayName) {
+		if ($this->is_html(strtolower($value)) !== false) {
+			// HTML Detected!
+			$this->vulnDetectedHTML($Method, "HTML CHARS", $DisplayName, 'XSS');
+		}
+	}
+	function arrayValues($Array) {
+		return array_values($Array);
+	}
+	function checkGET() {
+		foreach ($_GET as $key => $value) {
+			if (is_array($value)) {
+				$flattened = $this->arrayFlatten($value);
+				foreach ($flattened as $sub_key => $sub_value) {
+					$this->sqlCheck($sub_value, "_GET", $sub_key);
+					$this->xssCheck($sub_value, "_GET", $sub_key);
+					$this->htmlCheck($sub_value, "_GET", $sub_key);
+				}
+			} else {
+				$this->sqlCheck($value, "_GET", $key);
+				$this->xssCheck($value, "_GET", $key);
+				$this->htmlCheck($value, "_GET", $key);
+			}
+		}
+	}
+	function checkPOST() {
+		foreach ($_POST as $key => $value) {
+			if (is_array($value)) {
+				$flattened = $this->arrayFlatten($value);
+				foreach ($flattened as $sub_key => $sub_value) {
+					$this->sqlCheck($sub_value, "_POST", $sub_key);
+					$this->xssCheck($sub_value, "_POST", $sub_key);
+					$this->htmlCheck($sub_value, "_POST", $sub_key);
+				}
+			} else {
+				$this->sqlCheck($value, "_POST", $key);
+				$this->xssCheck($value, "_POST", $key);
+				$this->htmlCheck($value, "_POST", $key);
+			}
+		}
+	}
+	function checkCOOKIE() {
+		foreach ($_COOKIE as $key => $value) {
+			if (is_array($value)) {
+				$flattened = $this->arrayFlatten($value);
+				foreach ($flattened as $sub_key => $sub_value) {
+					$this->sqlCheck($sub_value, "_COOKIE", $sub_key);
+					$this->xssCheck($sub_value, "_COOKIE", $sub_key);
+					$this->htmlCheck($sub_value, "_COOKIE", $sub_key);
+				}
+			} else {
+				$this->sqlCheck($value, "_COOKIE", $key);
+				$this->xssCheck($value, "_COOKIE", $key);
+				$this->htmlCheck($value, "_COOKIE", $key);
+			}
+		}
+	}
+	function gua() {
+		if (isset($_SERVER['HTTP_USER_AGENT'])) {
+			return $_SERVER['HTTP_USER_AGENT'];
+		}
+		return md5(rand());
+	}
+	function cutGua($string) {
+		$five = substr($string, 0, 4);
+		$last = substr($string, -3);
+		return md5($five.$last);
+	}
+	function getCSRF() {
+		if (isset($_SESSION['token'])) {
+			$token_age = time() - $_SESSION['token_time'];
+			if ($token_age <= 300){    /* Less than five minutes has passed. */
+				return $_SESSION['token'];
+			} else {
+				$token = md5(uniqid(rand(), TRUE));
+				$_SESSION['token'] = $token . "asd648" . $this->cutGua($this->gua());
+				$_SESSION['token_time'] = time();
+				return $_SESSION['token'];
+			}
+		} else {
+			$token = md5(uniqid(rand(), TRUE));
+			$_SESSION['token'] = $token . "asd648" . $this->cutGua($this->gua());
+			$_SESSION['token_time'] = time();
+			return $_SESSION['token'];
+		}
+	}
+	function verifyCSRF($Value) {
+		if (isset($_SESSION['token'])) {
+			$token_age = time() - $_SESSION['token_time'];
+			if ($token_age <= 300){    /* Less than five minutes has passed. */
+				if ($Value == $_SESSION['token']) {
+					$Explode = explode('asd648', $_SESSION['token']);
+					$gua = $Explode[1];
+					if ($this->cutGua($this->gua()) == $gua) {
+						// Validated, Done!
+						unset($_SESSION['token']);
+						unset($_SESSION['token_time']);
+						return true;
+					}
+					unset($_SESSION['token']);
+					unset($_SESSION['token_time']);
+					return false;
+				}
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	function useCloudflare() {
+		$this->IPHeader = "HTTP_CF_CONNECTING_IP";
+	}
+	function useBlazingfast() {
+		$this->IPHeader = "X-Real-IP";
+	}
+	function customIPHeader($String = 'REMOTE_ADDR') {
+		$this->IPHeader = $String;
+	}
+	function start() {
+		session_start();
+		$this->checkGET();
+		$this->checkPOST();
+		$this->checkCOOKIE();
+	}
+
+}
+?>
